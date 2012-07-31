8c65dcaddde5de1b7693156012ebcd5704b5db37
Geng Shuang <gengs@neusoft.com>
2012-07-31 17:44:32 +0800

    增加进度条显示。

--- pfc.pl
+++ pfc.pl
@@ -16,12 +16,19 @@ my @pre_files = $ORI_PATH eq $PRE_PATH ? @ori_files : file_list($PRE_PATH);
 print "Compare ".@ori_files." files to ".@pre_files." files.\n";
 
 my %same_files;
-
+my $o_count = 0;
+$| = 1;
 foreach my $ori_file (@ori_files) {
+    $o_count++;
+    my $percent = processPer(scalar @ori_files, $o_count);
     my $o_file = $ORI_PATH.$ori_file;
     my $o_file_size = -s $o_file;
 
+    my $p_count = 0;
     foreach my $pre_file (@pre_files) {
+        $p_count++;
+        my $bar = processBar(scalar @pre_files, $p_count);
+        print "\r", " " x 60, "\r", $bar, $percent;
         my $p_file = $PRE_PATH.$pre_file;
         next if $p_file eq $o_file;
         my $p_file_size = -s $p_file;
@@ -39,6 +46,8 @@ foreach my $ori_file (@ori_files) {
         }
     }
 }
+print "\n";
+$| = 0;
 
 foreach my $file_hash (keys %same_files) {
     print join("\n", (@{$same_files{$file_hash}}))."\n\n";
@@ -78,4 +87,23 @@ sub file_list {
     }
 
     @file_list;
+}
+
+sub processPer {
+    my $total = shift;
+    my $count = shift;
+
+    my $per = sprintf " %5.2f%%", $count/$total*100;
+    return $per;
+}
+
+sub processBar {
+    my $total = shift;
+    my $count = shift;
+    my $LENGTH = 50;
+
+    my $barcount = int($count / $total * 50);
+    my $bar = "|" x $barcount;
+    $bar .= " " x (50 - $barcount);
+    return "[$bar]";
 }
